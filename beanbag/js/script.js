f6cbd281694c278225581d6e3bce045d6652a5a7
Kirill Romash <kirillromash84@gmail.com>
2016-11-13 23:54:15 +0200

    Add files via upload

---
+++ beanbag/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('#slides').slides({
+    play: 5000,
+    pause: 2500,
+    hoverPause: true
+  });
+});
+
+$(document).ready(function(){
+  $('.slider5').bxSlider({
+    slideWidth: 256,
+    minSlides: 3,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 55
+  });
+});
